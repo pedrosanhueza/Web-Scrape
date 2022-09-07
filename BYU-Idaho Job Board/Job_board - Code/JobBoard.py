2d6d9ad53c6ea3e3cfd1d7774d6aa971354bf295
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-09-07 01:36:41 -0600

    commit

--- BYU-Idaho Job Board/Job_board - Code/JobBoard.py
+++ BYU-Idaho Job Board/Job_board - Code/JobBoard.py
@@ -5,7 +5,8 @@ import requests
 import pandas as pd
 import streamlit as st
 from bs4 import BeautifulSoup
-import numpy as np
+from datetime import datetime, timedelta
+import time
 
 # ------------------------ STREAMLIT ------------------------------------------------------------------------------------------------
 
@@ -27,6 +28,8 @@ col_dates = ['dateUpdated','startDate','endDate','beginningDate','recruitingStar
 
 data[col_dates] = data[col_dates].astype('datetime64[ns]')
 
+data.dateUpdated = data.dateUpdated.apply(lambda x: datetime.strftime(x, "%Y-%m-%d"))
+
 data['URL'] = data.jobID.apply(lambda x: f'https://web.byui.edu/StudentEmployment/job/{x}')
 
 columns_to_drop = [
@@ -34,7 +37,7 @@ columns_to_drop = [
     # 'description', # not needed for EDA
     'summary', # not needed for EDA
     'displayJob', # single boolean
-    'dateUpdated', # not needed for display
+    # 'dateUpdated', # not needed for display
     # 'startDate',
     # 'endDate', # not needed for display
     'approximateHoursPerWeek', # not consistent
@@ -60,33 +63,56 @@ data['description'] = data['description'].apply(lambda x: [p.text.strip() for p 
 
 st.sidebar.write('# FILTERS')
 
+date_input = st.sidebar.date_input("Jobs posted on",datetime.today())
+
+d = str(date_input.strftime("%Y-%m-%d"))
+
 # ------------------------ STREAMLIT KPI GENERAL ------------------------------------------------------------------------------------------------
 
 '## General KPI\'s'
 
 KPI1_jobs = round(1-(400/data.shape[0]),2)
 
-
 KPI1_1_max = round(1-(data.payRate.median()/data.payRate.max()),2)
 
 jobs_not_online = data[~data.title.str.contains('Online')].shape[0]
 
-KPI1,KPI1_1,KPI_K,KPI2, KPI3 = st.columns(5)
+KPI1,KPI1_1,KPI2 = st.columns(3)
 
 KPI1.metric("Amount of Jobs", f"{data.shape[0]}")
 
 KPI1_1.metric("Highest Pay Rate Job", f"${data.payRate.max()}")
 
-KPI_K.metric("Jobs not Online", f"{jobs_not_online}")
-
 KPI2.metric("Managers Recluting", f"{data.managerName.nunique()}")
 
-KPI3.metric("Departments", f"{data.departmentName.nunique()}")
+today = data[data.dateUpdated == time.strftime("%Y-%m-%d")].shape[0]
+
+yesterday = data[data.dateUpdated == (datetime.today() - timedelta(1)).strftime("%Y-%m-%d")].shape[0]
+
+KPI3, KPI4, KPI_K = st.columns(3)
+
+KPI3.metric("Departments hiring", f"{data.departmentName.nunique()}")
+
+KPI4.metric("Jobs posted today",today, f'{round((today/yesterday)-1,2)}% of yesterday')
+
+KPI_K.metric("Jobs not Online", f"{jobs_not_online}")
+
+# ------------------------ STREAMLIT  ----------------------------------------------------)--------------------------------------------
+
+f"## Jobs posted on {date_input.strftime('%b %d, %Y')}"
+
+f'Jobs posted: {data[data.dateUpdated == d].shape[0]}'
+
+st.table(data[data.dateUpdated == d][['title','payRate','departmentName','URL']])
+
+# ------------------------ STREAMLIT TABLE ------------------------------------------------------------------------------------------------
 
 st.dataframe(data)
 
+# ------------------------ STREAMLIT GRAPH ------------------------------------------------------------------------------------------------
+
 Chart_option = st.selectbox(
-     'How would you like to be contacted?',
+     'what would you like to see?',
      ('Pay Rate Bar Char', 'Specific Job', 'Mobile phone'))
 
 df1 = data.payRate.value_counts().reset_index().rename(columns={"index": "Hourly Pay", "payRate": "Amount of Jobs"})
@@ -117,3 +143,9 @@ df2,
 y='Amount of Jobs',
 x='endDate'
 )
+
+# TO DO
+
+# I WANNA SEE TOP 3 HIGEST PY JOBS AND THEIR DETILS IN A ORGANIZED MANNER
+# GRAPH OF JOBS POSTED OVERTIME -- WHEN JOBS GET POST THE MOST
+# AVERAGE PAY RATE, VALUE COUNT
