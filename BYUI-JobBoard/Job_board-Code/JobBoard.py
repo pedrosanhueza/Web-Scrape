ad8279f1f12c1b7cbd954556f82912998065007c
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-09-15 13:39:22 -0600

    commit

--- BYUI-JobBoard/Job_board-Code/JobBoard.py
+++ BYUI-JobBoard/Job_board-Code/JobBoard.py
@@ -126,15 +126,15 @@ rate_1 = J_data['payRate'].iloc[0]
 rate_2 = J_data['payRate'].iloc[1]
 rate_3 = J_data['payRate'].iloc[2]
 
-url_1 = J_data['URL'].iloc[0]
-url_2 = J_data['URL'].iloc[1]
-url_3 = J_data['URL'].iloc[2]
+dept_1 = J_data['departmentName'].iloc[0]
+dept_2 = J_data['departmentName'].iloc[1]
+dept_3 = J_data['departmentName'].iloc[2]
 
 J1,J2,J3 = st.columns(3)
 
-J1.metric(title_1, rate_1, url_1)
-J2.metric('Title', J_data['payRate'].iloc[1],'[apply](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)')
-J3.metric('Title', J_data['payRate'].iloc[2],'[apply](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)')
+J1.metric(title_1, rate_1, dept_1)
+J2.metric(title_2, rate_2, dept_2)
+J3.metric(title_3, rate_3, dept_3)
 
 # ------------------------ STREAMLIT TABLE ------------------------------------------------------------------------------------------------
 
