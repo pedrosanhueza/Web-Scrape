c2e48ac09541d5664a16df609139f3cfc0b4e196
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-09-25 01:21:27 -0600

    commit

--- @Calling-Code/Billionaires.py
+++ @Calling-Code/Billionaires.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.write('Showing Billionaires Code here!')
 
-st. pd.DataFrame([{'A':1}])
+st.dataframe(pd.DataFrame([{'A':1}]))
 
 # st.dataFrame(
 #     pd.DateFrame({'a':1})
