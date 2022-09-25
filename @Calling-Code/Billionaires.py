9745a87e00f28e78c511680c83f457b4172c99b4
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-09-25 01:31:00 -0600

    commit

--- @Calling-Code/Billionaires.py
+++ @Calling-Code/Billionaires.py
@@ -6,11 +6,5 @@ st.write('Showing Billionaires Code here!')
 
 data = pd.DataFrame([{'A':1}])
 
-class Bill:
-  def __init__(self, data, url):
-    self.data = st.dataframe(data)
-    self.url = 'https://www.forbes.com/billionaires/'
-
-# st.dataFrame(
-#     pd.DateFrame({'a':1})
-# )
+def Bill(data):
+    st.dataframe(data)
