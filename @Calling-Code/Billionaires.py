add6955326b3f0e8cd027d3572ffca44372d966d
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-09-25 01:42:53 -0600

    commit

--- @Calling-Code/Billionaires.py
+++ @Calling-Code/Billionaires.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 data = pd.DataFrame([{'A':1}])
 
-def Bill(data):
+def dataFr():
     st.write('Text inside Bill function')
     st.dataframe(data)
     st.write('END Bill function')
