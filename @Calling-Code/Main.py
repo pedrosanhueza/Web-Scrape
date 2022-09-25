aecb607502a472bdb7d8091b5dc33387b29af4c2
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-09-25 00:32:28 -0600

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -1,9 +1,41 @@
 import streamlit as st
 
-if st.button('Run Script'):
-    
-    st.write('Start')
-    
-    import Root
-    
-    st.write('Done')
+st.write('# Web Scraping Projects')
+st.write('Author: Pedro Sanhueza')
+
+options = (
+    'SurplusSaleItems',
+    'Forbes-Universities',
+    'BYUI-JobBoard',
+    'BYU-Idaho Class Catalog',
+    'Forbes',
+    'News',
+    'Políticos Españoles',
+    'Irvine Spectrum Center',
+    'Politicos Chilenos',
+    'FIFA World Cup',
+    'SINCA MMA Gob',
+    'Mutual Feedback',
+    'US House of Representatives',
+    'Financial Data',
+    'Country Code',
+    'Billionaires')
+
+option = st.sidebar.selectbox('Webpages', options)
+
+st.write('You selected:', option)
+
+tab1, tab2, tab3, tab4 = st.tabs(['Page', 'Table', 'Code', 'Analysis'])
+
+with tab1:
+   st.header(option)
+   "[Open](https://alternativeto.net/) website"
+
+with tab2:
+   st.header("Table")
+
+with tab3:
+   st.header("Code")
+
+with tab4:
+   st.header("Analysis")
