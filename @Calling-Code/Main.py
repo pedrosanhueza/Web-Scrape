7c5e40138b4c5a7bc337e8fae1ab20c6623a2be7
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-09-26 14:51:49 -0600

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -1,10 +1,10 @@
 
-## ----------------------------------------- Import -------------------------------------------------- ##
+## ----------------------------------------- Import -------------------------------------------------------------------------------------- ##
 import streamlit as st
 
 from Projects import SurplusStore
 from Projects import catalogBYUI
-# from Projects import SurplusStore
+from Projects import representatives
 # from Projects import SurplusStore
 # from Projects import SurplusStore
 # from Projects import SurplusStore
@@ -19,26 +19,7 @@ st.write('''
     Description: Web Scraping EDA
     ''')
 
-project_id = {
-   'Billionaires'          :1,
-   'Class Catalog - BYUI'  :2,
-   'Country Code'          :3,
-   'FIFA World Cup'        :4,
-   'Financial Data'        :5,
-   'Forbes - Billionaries' :6,
-   'Forbes - Universities' :7,
-   'Irvine Spectrum Center':8,
-   'Job Board - BYUI'      :9,
-   'Mutual App Feedback'   :10,
-   'News - CBS'            :11,
-   'Politicos Chilenos'    :12,
-   'Políticos Españoles'   :13,
-   'SINCA MMA Gob'         :14,
-   'Surplus Store - BYUI'  :15,
-   'US House of Representatives':16,
-   'test'                  :17}
-
-## ----------------------------------------- Beggin Side Bar ----------------------------------------- ##
+## ----------------------------------------- Beggin Side Bar ----------------------------------------------------------------------------- ##
 
 options = (
    'Billionaires',
@@ -61,82 +42,14 @@ options = (
 
 option = st.sidebar.selectbox('Web Scraping Projects', options)
 
-## ----------------------------------------- Modules Variables ------------------------------------ ##
-
-st.components.v1.html('''
-<iframe src="https://www.house.gov/representatives" title="description"></iframe>
-''',height=600)
-
-
-if project_id[option] == 1:
-   from Projects import SurplusStore
-
-elif project_id[option] == 2:
-   from Projects import catalogBYUI
-
-elif project_id[option] == 3:
-   st.write('b')
-
-elif project_id[option] == 4:
-   st.write('b')
-
-elif project_id[option] == 5:
-   st.write('b')
-
-elif project_id[option] == 6:
-   st.write('b')
-
-elif project_id[option] == 7:
-   st.write('b')
-
-elif project_id[option] == 8:
-   st.write('b')
-
-elif project_id[option] == 9:
-   
-   from Projects import jobBoardBYUI
-
-   data = jobBoardBYUI.data
-
-   st.dataframe(data)
-
-elif project_id[option] == 10:
-   st.write('b')
-
-elif project_id[option] == 11:
-   st.write('b')
-
-elif project_id[option] == 12:
-   st.write('b')
-
-elif project_id[option] == 13:
-   st.write('b')
-
-elif project_id[option] == 14:
-   st.write('b')
-
-elif project_id[option] == 15:
-   st.write('SurplusStore')
-   
-   from Projects import SurplusStore
-
-
-
-elif project_id[option] == 16:
-   st.write('b')
-
-elif project_id[option] == 17:
-   st.write('b')
-
-else:
-   data = pd.DataFrame({'a':1})
-
-## ----------------------------------------- Modules Variables ------------------------------------ ##
+## ----------------------------------------- Modules Variables ------------------------------------------------------------------------ ##
 
 st.write('You selected:', option)
 
-if option == 'test':
-
+if option == 'US House of Representatives':
+   
+   data = representatives.data
+   
    st.dataframe(data)
 
 if option == 'Billionaires':
@@ -149,7 +62,7 @@ if option == 'Billionaires':
 
    st.write('Billionaires imported')
 
-## ----------------------------------------- Tables ----------------------------------------------- ##
+## ----------------------------------------- Tables ----------------------------------------------------------------------------------- ##
 
 tab1, tab2, tab3, tab4 = st.tabs(['Page', 'Table', 'Code', 'Analysis'])
 
--- @Calling-Code/Projects/representatives.py
+++ @Calling-Code/Projects/representatives.py
@@ -1,3 +1,23 @@
-import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-st.components.iframe()
+response = requests.get("https://www.house.gov/representatives")
+soup = BeautifulSoup(response.text, 'html.parser')
+
+tables = soup.select('table')
+[x.text.strip() for x in tables[0].select('td')][5::6]
+
+rows = []
+for table in tables[:56]:
+    row = {}
+    row['District']             = [x.text.strip() for x in table.select('td')][0::6]
+    row['Name']                 = [x.text.strip() for x in table.select('td')][1::6]
+    row['Party']                = [x.text.strip() for x in table.select('td')][2::6]
+    row['Office Room']          = [x.text.strip() for x in table.select('td')][3::6]
+    row['Phone']                = [x.text.strip() for x in table.select('td')][4::6]
+    row['Committee Assignment'] = [x.text.strip() for x in table.select('td')][5::6]
+    row['State']                = table.select_one('caption').text.strip()
+    rows.append(pd.DataFrame(row))
+
+data = pd.concat(rows)
