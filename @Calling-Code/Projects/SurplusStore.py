6afe985b29d3c51c1803d5fb3a233f2ce8178cfb
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-09-26 18:06:46 -0600

    commit

--- @Calling-Code/Projects/SurplusStore.py
+++ @Calling-Code/Projects/SurplusStore.py
@@ -1,12 +1,25 @@
-import pandas as pd
-import streamlit as st
+import requests # to get the html markdown from the url
+from bs4 import BeautifulSoup # to make the html readable
+import pandas as pd # to save table in a csv
 
-cat = 'Surplus Store Variable'
+# Get the data from website
+url = 'https://web.byui.edu/SurplusList/'
+html_data = requests.get(url)
+soup = BeautifulSoup(html_data.text, 'html.parser')
 
-dog = 'Surplus Store Variable 1'
+# Build Table
+rows=[]
+for item in soup.find_all('tr')[13:]:
+    row={}
+    row['Item Number'] = item.select('td')[0].text
+    row['Qty'] = item.select('td')[1].text
+    row['Description'] = item.select('td')[2].text
+    row['Condition'] = item.select('td')[3].text
+    row['Price'] = item.select('td')[4].text
+    row['Location'] = item.select('td')[5].text
+    row['Quick/Bid'] = item.select('td')[6].text
+    row['Image'] = item.select('td')[7].text
+    rows.append(row)
 
-data = pd.DataFrame(
-    {'col1': [1, 2], 'col2': [3, 4]}
-)
+data = pd.DataFrame(rows)
 
-st.write('@@@@@@@@')
