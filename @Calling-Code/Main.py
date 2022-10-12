6483e2e3be51f4865dab66e26bdc9e5461face55
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-10-12 17:14:57 -0600

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -156,6 +156,17 @@ elif projectOption[option] == 4:
       st.header(option)
    with tab4:
       st.header(option)
+   
+   page_bg_img = '''
+   <style>
+   body {
+   background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+   background-size: cover;
+   }
+   </style>
+   '''
+
+   st.markdown(page_bg_img, unsafe_allow_html=True)
 ## ----------------------------------------- Billionaires ------------------------------------------------------------------------ ##
 elif projectOption[option] == 15:
    data = SurplusStore.data
