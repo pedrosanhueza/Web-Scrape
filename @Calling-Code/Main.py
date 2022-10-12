a2094401dc1326d0382bff4de438b0bb0bce15fa
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-10-12 17:02:00 -0600

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -144,7 +144,7 @@ elif projectOption[option] == 4:
 
    with tab1:
       col2, col3, col4, col5, col6 = st.columns(5)
-      col2.metric("Players",    round(1 - ( data_main.shape[0] / data.shape[0] / data.Country.nunique())))
+      col2.metric("Players",    round(data_main.shape[0]))
       col3.metric("Age Avg",    round(data_main.AGE.mean()), str(Age_avg) + "%")
       col4.metric("Height Avg", round(data_main.HT.mean()), str(HT_avg)  + "%")
       col5.metric("Weight Avg", round(data_main.WT.mean()), str(WT_avg)  + "%")
