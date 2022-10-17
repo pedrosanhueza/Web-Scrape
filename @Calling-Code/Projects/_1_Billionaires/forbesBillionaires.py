d5fd756daa3c2490ae02653b0207542478507152
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-10-17 12:17:20 -0600

    commit

--- @Calling-Code/Projects/_1_Billionaires/forbesBillionaires.py
+++ @Calling-Code/Projects/_1_Billionaires/forbesBillionaires.py
@@ -2,69 +2,81 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-# url = "https://www.forbes.com/billionaires/page-data/index/page-data.json"
+url = "https://www.forbes.com/billionaires/page-data/index/page-data.json"
 
-# response = requests.get(url)
+response = requests.get(url)
 
-# json_data = response.json()
+json_data = response.json()
 
-# rows = json_data['result']['pageContext']['tableData']
+rows = json_data['result']['pageContext']['tableData']
 
-# tableData = {
-#     k: v
-#     for (k, v) in rows[0].items()
-#     if
-#     (k != 'person') & # dict
-#     (k != 'employment') & # dict
-#     (k != 'qas') & # two dicts
-#     (k != 'bios') & # list
-#     (k != 'abouts') & #list
-#     (k != 'csfDisplayFields') #list
-# }
+tableData = {
+    k: v
+    for (k, v) in rows[0].items()
+    if
+    (k != 'person') & # dict
+    (k != 'employment') & # dict
+    (k != 'qas') & # two dicts
+    (k != 'bios') & # list
+    (k != 'abouts') & #list
+    (k != 'csfDisplayFields') #list
+}
 
-# ls_=[]
+ls_=[]
 
-# for row in rows:
-#     tableData = {
-#         k: v
-#         for (k, v) in row.items()
-#         if
-#         (k != 'person') & # dict
-#         (k != 'employment') & # dict
-#         (k != 'qas') & # two dicts
-#         (k != 'bios') & # list
-#         (k != 'abouts') & #list
-#         (k != 'csfDisplayFields') #list
-#         }
-#     ls_.append(tableData)
+for row in rows:
+    tableData = {
+        k: v
+        for (k, v) in row.items()
+        if
+        (k != 'person') & # dict
+        (k != 'employment') & # dict
+        (k != 'qas') & # two dicts
+        (k != 'bios') & # list
+        (k != 'abouts') & #list
+        (k != 'csfDisplayFields') #list
+        }
+    ls_.append(tableData)
 
-# data = pd.DataFrame(ls_)
+data = pd.DataFrame(ls_)
 
-# cols_to_drop = [
-#     'parentListUri',
-#     'organization',
-#     'title',
-#     'selfMadeRank',
-#     'residenceStateRegion',
-#     'embargo',
-#     'residenceMsa',
-#     'impactInvestor',
-#     'numberOfSiblings',
-#     'numberOfSiblingsEst',
-#     'bio',
-#     # 'totalCharitableDonation',
-#     # 'charitableDonationPercentage',
-#     'thumbnail',
-#     'notableDeal',
-#     'valueCreated',
-#     'primaryIndustry',
-#     'portraitImage',
-#     'landscapeImage',
-#     'clients']
+cols_to_drop = [
+    'parentListUri',
+    'organization',
+    'title',
+    'selfMadeRank',
+    'residenceStateRegion',
+    'embargo',
+    'residenceMsa',
+    'impactInvestor',
+    'numberOfSiblings',
+    'numberOfSiblingsEst',
+    'bio',
+    # 'totalCharitableDonation',
+    # 'charitableDonationPercentage',
+    'thumbnail',
+    'notableDeal',
+    'valueCreated',
+    'primaryIndustry',
+    'portraitImage',
+    'landscapeImage',
+    'clients']
 
-# data.drop(cols_to_drop, axis=1, inplace=True)
+data.drop(cols_to_drop, axis=1, inplace=True)
 
-data = pd.DataFrame(range(20))
+data['squareImage'].fillna('https://i.forbesimg.com/media/assets/forbes_1200x1200.jpg', inplace=True)
+
+data['country'].fillna('Not Specified', inplace=True)
+
+data['state'].fillna('Not Specified', inplace=True)
+
+data['city'].fillna('Not Specified', inplace=True)
+
+data['gender'].fillna('Not Specified', inplace=True)
+
+data['birthDate'].fillna('Not Specified', inplace=True)
+
+data['firstName'].fillna('Not Specified', inplace=True)
 
 # --------------------------------------------------------------------
 
@@ -134,4 +146,19 @@ cols_to_drop = [
     'clients']
 
 data.drop(cols_to_drop, axis=1, inplace=True)
+
+data['squareImage'].fillna('https://i.forbesimg.com/media/assets/forbes_1200x1200.jpg', inplace=True)
+
+data['country'].fillna('Not Specified', inplace=True)
+
+data['state'].fillna('Not Specified', inplace=True)
+
+data['city'].fillna('Not Specified', inplace=True)
+
+data['gender'].fillna('Not Specified', inplace=True)
+
+data['birthDate'].fillna('Not Specified', inplace=True)
+
+data['firstName'].fillna('Not Specified', inplace=True)
+
 """
