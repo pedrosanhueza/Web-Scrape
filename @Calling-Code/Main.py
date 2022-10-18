a10e54a941be2b906233400e6ecb7f838cd4967e
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-10-18 14:38:29 -0600

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -225,12 +225,12 @@ elif projectOption[option] == 9:
 
    # tab1, tab2, tab3, tab4 = st.tabs(['Salary Analysis','Jobs not online','Table','Code'])
 
-   data_top5 = data[data.payRate == data.payRate.max()].head(5)
+   data_top5 = data.sort_values('payRate', ascending=False)
 
    for job in range(5):
       st.markdown(f'''
       <p>
-         <b> Job Title: </b> &emsp;&emsp; {data_top5.iloc[job].title}
+         <b> Job Title: </b> &emsp;&emsp;&emsp; {data_top5.iloc[job].title}
          <br>
          <b> Hourly Wage: </b> &emsp;&emsp; {data_top5.iloc[job].payRate}
          <br>
