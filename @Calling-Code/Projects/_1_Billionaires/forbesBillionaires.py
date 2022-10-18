0a94ce4b37626dcd56d432cf7b787b0f82d63377
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-10-18 11:09:24 -0600

    commit1

--- @Calling-Code/Projects/_1_Billionaires/forbesBillionaires.py
+++ @Calling-Code/Projects/_1_Billionaires/forbesBillionaires.py
@@ -10,25 +10,25 @@ json_data = response.json()
 
 rows = json_data['result']['pageContext']['tableData']
 
-ls_=[]
+# ls_=[]
 
-for row in rows:
-    tableData = {
-        k: v
-        for (k, v) in row.items()
-        if
-        (k != 'person') & # dict
-        (k != 'employment') & # dict
-        (k != 'qas') & # two dicts
-        (k != 'bios') & # list
-        (k != 'abouts') & #list
-        (k != 'csfDisplayFields') #list
-        }
-    ls_.append(tableData)
+# for row in rows:
+#     tableData = {
+#         k: v
+#         for (k, v) in row.items()
+#         if
+#         (k != 'person') & # dict
+#         (k != 'employment') & # dict
+#         (k != 'qas') & # two dicts
+#         (k != 'bios') & # list
+#         (k != 'abouts') & #list
+#         (k != 'csfDisplayFields') #list
+#         }
+#     ls_.append(tableData)
 
 # data = pd.DataFrame(ls_)
 
-data = pd.DataFrame([tableData])
+data = pd.DataFrame(rows)
 
 # cols_to_drop = [
 #     'parentListUri',
