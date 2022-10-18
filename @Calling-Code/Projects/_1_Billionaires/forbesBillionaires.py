58b997b1e3d0f013887054a8fcc710fb156c7ab3
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-10-18 11:13:21 -0600

    commit

--- @Calling-Code/Projects/_1_Billionaires/forbesBillionaires.py
+++ @Calling-Code/Projects/_1_Billionaires/forbesBillionaires.py
@@ -10,65 +10,61 @@ json_data = response.json()
 
 rows = json_data['result']['pageContext']['tableData']
 
-# ls_=[]
+ls_=[]
 
-# for row in rows:
-#     tableData = {
-#         k: v
-#         for (k, v) in row.items()
-#         if
-#         (k != 'person') & # dict
-#         (k != 'employment') & # dict
-#         (k != 'qas') & # two dicts
-#         (k != 'bios') & # list
-#         (k != 'abouts') & #list
-#         (k != 'csfDisplayFields') #list
-#         }
-#     ls_.append(tableData)
+for row in rows:
+    tableData = {
+        k: v
+        for (k, v) in row.items()
+        if
+        (k != 'person') & # dict
+        (k != 'employment') & # dict
+        (k != 'qas') & # two dicts
+        (k != 'bios') & # list
+        (k != 'abouts') & #list
+        (k != 'csfDisplayFields') #list
+        }
+    ls_.append(tableData)
 
-# data = pd.DataFrame(ls_)
+data = pd.DataFrame(ls_)
 
-data = pd.DataFrame(rows)
+cols_to_drop = [
+    'parentListUri',
+    'organization',
+    'title',
+    'selfMadeRank',
+    'residenceStateRegion',
+    'embargo',
+    'residenceMsa',
+    'impactInvestor',
+    'numberOfSiblings',
+    'numberOfSiblingsEst',
+    'bio',
+    # 'totalCharitableDonation',
+    # 'charitableDonationPercentage',
+    'thumbnail',
+    'notableDeal',
+    'valueCreated',
+    'primaryIndustry',
+    'portraitImage',
+    'landscapeImage',
+    'clients']
 
-# cols_to_drop = [
-#     'parentListUri',
-#     'organization',
-#     'title',
-#     'selfMadeRank',
-#     'residenceStateRegion',
-#     'embargo',
-#     'residenceMsa',
-#     'impactInvestor',
-#     'numberOfSiblings',
-#     'numberOfSiblingsEst',
-#     'bio',
-#     # 'totalCharitableDonation',
-#     # 'charitableDonationPercentage',
-#     'thumbnail',
-#     'notableDeal',
-#     'valueCreated',
-#     'primaryIndustry',
-#     'portraitImage',
-#     'landscapeImage',
-#     'clients']
+data.drop(cols_to_drop, axis=1, inplace=True)
 
-# data.drop(cols_to_drop, axis=1, inplace=True)
+data['squareImage'].fillna('https://i.forbesimg.com/media/assets/forbes_1200x1200.jpg', inplace=True)
 
-# data['squareImage'].fillna('https://i.forbesimg.com/media/assets/forbes_1200x1200.jpg', inplace=True)
+data['country'].fillna('Not Specified', inplace=True)
 
-# data['country'].fillna('Not Specified', inplace=True)
+data['state'].fillna('Not Specified', inplace=True)
 
-# data['state'].fillna('Not Specified', inplace=True)
+data['city'].fillna('Not Specified', inplace=True)
 
-# data['city'].fillna('Not Specified', inplace=True)
+data['gender'].fillna('Not Specified', inplace=True)
 
-# data['gender'].fillna('Not Specified', inplace=True)
+data['birthDate'].fillna('Not Specified', inplace=True)
 
-# data['birthDate'].fillna('Not Specified', inplace=True)
-
-# data['firstName'].fillna('Not Specified', inplace=True)
-
-# data = pd.DataFrame(range(20))
+data['firstName'].fillna('Not Specified', inplace=True)
 
 # --------------------------------------------------------------------
 
