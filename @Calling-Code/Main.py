f1c46ba1e7382f2fd4c10eb2f70d3cac2afa2394
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-10-18 14:41:36 -0600

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -236,7 +236,7 @@ elif projectOption[option] == 9:
          <br>
          <b> Recluter: </b> &emsp;&emsp;&emsp;&emsp; {data_top5.iloc[job].managerName}
          <br>
-         <b> Department: </b> &emsp;&emsp;&emsp;{data_top5.iloc[job].departmentName}
+         <b> Department: </b> &emsp;&emsp; {data_top5.iloc[job].departmentName}
          <br>
       <p>
       ''',unsafe_allow_html=True)
