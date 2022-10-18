5bf8dbbf6aab4a75075d267d7411bbdcfa820abb
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-10-18 15:28:52 -0600

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -186,6 +186,8 @@ if projectOption[option] == 1: #'Billionaires'
 
 # ## ----------------------------------------- Job Board - BYUI ------------------------------------------------------------------------ ##
 elif projectOption[option] == 9:
+
+   st.image('https://brightspotcdn.byui.edu/dims4/default/3b59bf0/2147483647/strip/true/crop/560x314+22+0/resize/852x478!/format/webp/quality/90/?url=http%3A%2F%2Fbyu-idaho-brightspot.s3.amazonaws.com%2F6c%2Fa1%2Fb3306ea14ed5aef8b82a86ee39d0%2Fbyui-offical-logo-blue-01.png')
    
    st.markdown('''
    <center>
