f09bba78a2bfe807e89c1d81fe79ff6e165e1837
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-10-21 12:20:38 -0600

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -282,8 +282,6 @@ elif projectOption[option] == 9:
 
       st.dataframe(yesterday_data[['title','payRate','departmentName','managerName','URL']].sort_values('payRate',ascending=False))
    
-   st.dataframe(yesterday_data[['title','payRate','departmentName','managerName','URL']].sort_values('payRate',ascending=False))
-
    st.markdown('''
    <br>
    <p style="font-size:40px;text-align:center;color:#4F9ACF;">
