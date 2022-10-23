1fb79c331e6116dc00c067503ff453c9f7634e10
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-10-22 23:49:10 -0600

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -542,7 +542,7 @@ elif projectOption[option] == 16:
     x=0.67))
    st.plotly_chart(fig,use_container_width=True)
 
-   st.bar_chart(data.Party)
+   st.bar_chart(data, x='State')
 
    party_group = st.radio("Representative Members by State",('Both','Republicans','Democrats'), horizontal=True)
 
