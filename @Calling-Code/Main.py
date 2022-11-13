2cdfa4ae2e2d6e6acaae4e7cbd3761fde78368b1
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-11-13 00:26:24 -0700

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -106,7 +106,7 @@ if projectOption[project] == 2:
    import streamlit.components.v1 as components
 
    # embed streamlit docs in a streamlit app
-   components.iframe("https://docs.streamlit.io/en/latest",width=800, height=1000, scrolling=True)
+   components.iframe("{url}",width=800, height=1000, scrolling=True)
 
 # ## ----------------------------------------- # Country Code ------------------------------------------------------------------------ ##
 if projectOption[project] == 3:
