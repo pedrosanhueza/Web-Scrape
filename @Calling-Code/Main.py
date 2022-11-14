dcdaa6d398a62fc85eb4ce3d687d63079510394b
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-11-13 22:47:04 -0700

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -791,9 +791,8 @@ if projectOption[project] == 17:
    with st.expander("See Website 👨🏻‍💻"):
       col1, col2, col3 = st.columns([1,3,1])
       with col1:
-         st.write(' ')
-      with col2:
          st.markdown(f'''<a href={url}>{url}</a>''',unsafe_allow_html=True)
+      with col2:
          components.iframe(f"{url}", width=350, height=500, scrolling=True)
       with col3:
          st.write(' ')
