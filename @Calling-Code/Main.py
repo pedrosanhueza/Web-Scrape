1f3f3298a6f28c2b500e289b6365b29e7b563690
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-11-14 12:43:13 -0700

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -531,7 +531,7 @@ if projectOption[project] == 12:
    data_1 = data_simple
    st.dataframe(data_1)
 
-   url_img = 'https://www.camara.cl/img.aspx?prmID=GRCL1096'
+   url_img = "https://www.camara.cl/img.aspx?prmID=GRCL1096"
 
    st.markdown(f'''
    <center>
@@ -539,7 +539,7 @@ if projectOption[project] == 12:
    </center>
    ''',unsafe_allow_html=True)
 
-   st.image('https://www.camara.cl/img.aspx?prmID=GRCL1096')
+   st.image(f'{url_img}')
 
    # if st.button('Run Complete table'):
    #    from Projects._12_PoliticosChilenos.camara_diputados import data
