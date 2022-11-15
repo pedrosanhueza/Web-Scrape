04b8053bd7cd8d9f120db96f1f571ce889362954
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-11-15 01:03:29 -0700

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -33,11 +33,11 @@ st.markdown('''
 
 col1, col2, col3 = st.columns([1,3,1])
 with col1:
-    st.write(' ')
+   st.write(' ')
 with col2:
-   st.image('@Calling-Code/Pictures/Me_sticker.png', caption='Profile Sticker', width=150) 
+   st.write(' ')
 with col3:
-    st.write(' ')
+   st.image('@Calling-Code/Pictures/Me_sticker.png', caption='Profile Sticker', width=150) 
 
 st.markdown('''
 <p style="font-size:20px;text-align:center;">
