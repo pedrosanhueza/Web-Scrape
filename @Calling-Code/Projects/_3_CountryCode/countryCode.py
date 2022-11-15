948a26c221dbcbd7464ba3ea36521b8fd6828b4c
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-11-15 01:47:58 -0700

    commit

--- @Calling-Code/Projects/_3_CountryCode/countryCode.py
+++ @Calling-Code/Projects/_3_CountryCode/countryCode.py
@@ -1,6 +1,6 @@
 import requests # to download html source from url
 from bs4 import BeautifulSoup # find elements in html
-import pandas as pd # build data frame
+import pandas as pd # build dataframe
 
 ### Webscrape URL
 
@@ -12,12 +12,12 @@ ls = [x.get_text() for x in soup.select('td')][:240*6] # collect all values of t
 
 ### Build Data Table
 json_list = { # get the 6th item in list starting from 1, 2, 3, 4, 5, and 6th element
-'Country' :ls[0::6],
+'Country'      :ls[0::6],
 'Country_code' : ls[1::6],
-'ISO_codes' : ls[2::6],
-'Population' : ls[3::6],
-'Area_KM2' : ls[4::6],
-'GDP_USD' : ls[5::6]}
+'ISO_codes'    : ls[2::6],
+'Population'   : ls[3::6],
+'Area_KM2'     : ls[4::6],
+'GDP_USD'      : ls[5::6]}
 
 data = pd.DataFrame(json_list) # build data frame
 
