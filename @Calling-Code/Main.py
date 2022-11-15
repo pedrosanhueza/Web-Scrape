35832b375674b7f622c6b696c2ebab9e69e87ff4
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-11-15 01:07:22 -0700

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -67,7 +67,7 @@ st.markdown('''
 st.markdown('''<hr>''',unsafe_allow_html=True)
 
 st.markdown('''
-<img src='https://raw.githubusercontent.com/pedrosanhueza/Web_Scrape/main/%40Calling-Code/Pictures/Me_sticker.png align="left" >
+<img src='https://raw.githubusercontent.com/pedrosanhueza/Web_Scrape/main/%40Calling-Code/Pictures/Me_sticker.png >
 <p> Totally </p>
 ''',unsafe_allow_html=True)
 
