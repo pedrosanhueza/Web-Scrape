4c1af7cc3edb6970e33057a1f453c974f2a2bc78
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-11-15 01:02:56 -0700

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -31,6 +31,14 @@ st.markdown('''
 </p>
 ''',unsafe_allow_html=True)
 
+col1, col2, col3 = st.columns([1,3,1])
+with col1:
+    st.write(' ')
+with col2:
+   st.image('@Calling-Code/Pictures/Me_sticker.png', caption='Profile Sticker', width=150) 
+with col3:
+    st.write(' ')
+
 st.markdown('''
 <p style="font-size:20px;text-align:center;">
    Projects are listed in the drop down bar. Each project has:
@@ -39,16 +47,6 @@ st.markdown('''
    <sup> 3 </sup> table with data extracted.
 </p>
 ''',unsafe_allow_html=True)
-
-st.markdown('''
-<img src= @Calling-Code/Pictures/Me_sticker.png align="left" >
-   <sup> 1 </sup> A code source, an
-   <sup> 2 </sup> embedded website or the website hyperlink, and a
-   <sup> 3 </sup> table with data extracted.
-''',unsafe_allow_html=True)
-
-
-
 
 st.markdown('''
 <p style="font-size:12px;text-align:left;color:rgba(192,192,192,0.3);">
