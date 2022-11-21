e6f86aafc7c89d460cb215cde47103d8f7acd5e9
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-11-20 23:28:22 -0700

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -115,7 +115,7 @@ if projectOption[project] == 1:
    </p>
    ''',unsafe_allow_html=True)
 
-   st.image('@Calling-Code/Pictures/API_call_1.gif', caption='Scraping Process', width=300) 
+   st.image('@Calling-Code/Pictures/API_call_1.gif', caption='Scraping Process') 
 
 ## ----------------------------------------- Class Catalog - BYUI ------------------------------------------------------------------------ ##
 if projectOption[project] == 2:
