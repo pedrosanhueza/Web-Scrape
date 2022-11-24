a10357e01091e4756909150e59b756308f6bab0a
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-11-23 18:02:17 -0700

    commit_cbsnews

--- @Calling-Code/Projects/_11_NewsCBS/cbsnews.py
+++ @Calling-Code/Projects/_11_NewsCBS/cbsnews.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd 
 
-url = 'https://www.cbsnews.com/world/'
+url_main = 'https://www.cbsnews.com/world/'
 
-response = requests.get(url)
+response = requests.get(url_main)
 
 soup = BeautifulSoup(response.text)
 
@@ -31,3 +31,41 @@ for section in sections:
         pass
 
 data = pd.DataFrame(rows)
+
+# --------------------------------- Script 1 
+
+script_1 = '''
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd 
+
+url_main = 'https://www.cbsnews.com/world/'
+
+response = requests.get(url_main)
+
+soup = BeautifulSoup(response.text)
+
+rows = [] # list of information for each article
+
+sections = soup.find_all('section')
+
+for section in sections:
+
+    articles = section.find_all('article')
+    
+    try:
+        # print(section.find('h3').text.strip())
+        for article in articles:
+            row = {}
+            row['Section'] = section.find('h3').text.strip()
+            row['Title'] = article.find('h4').text.strip()
+            row['Description'] = article.find('p').text.strip()
+            row['Date'] = article.find('li').text
+            row['url_article'] = article.find('a')['href']
+            row['url_image'] = article.find('img')['src']
+            rows.append(row)
+    except:
+        pass
+
+data = pd.DataFrame(rows)
+'''
