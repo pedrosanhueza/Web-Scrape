4754c2289295f7aad88be7c33936a19b6b42f48a
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-11-25 10:29:32 -0700

    commit

--- @Calling-Code/Projects/_18_SigEp_Chapters/SigEp.py
+++ @Calling-Code/Projects/_18_SigEp_Chapters/SigEp.py
@@ -2,7 +2,7 @@ import requests
 
 import pandas as pd
 
-url_main = 'https://sigep.org/'
+url_main = 'https://sigep.org/chapters/'
 
 # headers of this particular server
 headers = {'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"}
