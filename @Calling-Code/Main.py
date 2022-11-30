ec5bf43c21ddea20a88a5ce4f010ea4ac3a283d9
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-11-29 23:49:58 -0700

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -1194,4 +1194,26 @@ if projectOption[project] == 19:
          
       st.dataframe(data)
 
+   KPI1,KPI2,KPI3,KPI4,KPI5,KPI6 = st.columns(6)
+   KPI1.metric('Apps', data.shape[0])
+   KPI2.metric('Developers', data.developer.nunique())
+   KPI3.metric('Free Apps', data[data.pricing.str.contains('Free')].shape[0])
+   KPI4.metric('Trial Period Apps', data[data.pricing.str.contains('Trial Period')].shape[0])
+   KPI5.metric('One-Time charge', data[data.pricing.str.contains('One-Time Charge')].shape[0])
+   KPI6.metric('Paid Apps', data[data.pricing.str.contains('One-Time Charge')].shape[0])
+
+   data.loc[data['pricing'].str.contains('Free'), 'Payment'] = 'Free'
+   data.loc[data['pricing'].str.contains('trial'), 'Payment'] = 'Trial Period'
+   data.loc[data['pricing'].str.contains('one-time charge'), 'Payment'] = 'One-Time Charge'
+   data.loc[data['pricing'].str.contains('month'), 'Payment'] = 'Paid Apps'
+   data.loc[data['pricing'].str.contains('year'), 'Payment'] = 'Paid Apps'
+
+   st.bar_chart(
+      data=data.Payment.value_counts().reset_index().rename(columns={'index': 'Method'}),
+      x='Method',
+      y='Payment'
+   )
+
    st.markdown(f'''<br><br><br><br><br>''',unsafe_allow_html=True)
+
+
