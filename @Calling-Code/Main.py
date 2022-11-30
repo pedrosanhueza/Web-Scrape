df3a7bd3edcccebea653c4afacfb8af7aded068a
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-11-30 13:41:09 -0700

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -142,7 +142,8 @@ if projectOption[project] == 2:
             The
             <a href='{url}' style="color:#4F9ACF;" >University's catalog </a>
             website has the list of all classes offered and its Course ID, Title, Activation Date, Department, and more.
-            The university currenlt has <b> {data.shape[0]} </b> open courses from where the students can register to (including online classes) <b> {data.shape[1]} </b> descriptions for each course.
+            The university currenlt has <b> {data.shape[0]} </b> open courses from where the students can register to (including online classes).
+            It also has <b> {data.shape[1]} </b> main academic departments.
             <br>
             This code extract all that data and puts it into a local CSV file.
          </p>
