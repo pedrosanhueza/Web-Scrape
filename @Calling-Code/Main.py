d17e71e97f0512b102e466bb5f580562672958c0
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-12-01 12:50:06 -0700

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -25,7 +25,8 @@ col1, col2, col3 = st.columns([2,3,1])
 with col1:
    st.write(' ')
 with col2:
-   st.image('@Calling-Code/Pictures/Me_sticker.png', caption='Profile Sticker', width=300) 
+   # st.image('@Calling-Code/Pictures/Me_sticker.png', caption='Profile Sticker', width=300) 
+   st.image('@Calling-Code/Pictures/profile.jpg', caption='Profile Sticker', width=300)
 with col3:
    st.write(' ')
 
