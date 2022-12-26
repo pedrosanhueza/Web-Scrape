3ad18bf0b3bf8934a5bbb591d857e490b36adb60
Pedro Sanhueza <pedroasd1205@gmail.com>
2022-12-25 17:47:34 -0700

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -180,7 +180,7 @@ if projectOption[project] == 2:
 
       bars = alt.Chart(df).mark_bar().encode(
       x='Classes:Q',
-      y="Department:O")
+      y="Department:O",sort='-x')
 
    text = bars.mark_text(
       align='left',
