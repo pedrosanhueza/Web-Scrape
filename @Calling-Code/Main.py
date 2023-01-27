a3923fd21c2973aef060707f43f4fd4c4989b814
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-01-26 20:50:21 -0700

    commit

--- @Calling-Code/Main.py
+++ @Calling-Code/Main.py
@@ -45,6 +45,8 @@ with col3:
 
 st.markdown('''
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <p style="font-size:20px;text-align:center;">
 
    Select a project from the drop-down bar.
